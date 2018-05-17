6abe6ab8bab800464237a86fc39f7ab8f64dc8b3
CarlOlson <CarlOlson@users.noreply.github.com>
2018-05-17 00:15:58 -0500

    handle commands from server via dcg

--- main.pl
+++ main.pl
@@ -1,15 +1,28 @@
 
-:- initialization main.
+:- use_module(library(dcg/basics)).
+:- use_module(library(pio)).
+
+game --> commands, eos.
+
+commands --> command, commands.
+commands --> command.
+
+command --> settings.
+command --> update.
+command --> move.
+
+settings --> "settings", string_without("\n", _), "\n".
+
+update   --> "update", string_without("\n", _), "\n".
+
+move     -->  "action move ", integer(_Time), "\n",
+  {
+    moves(Moves),
+    random_member(Move, Moves),
+    format("~p~n", [Move])
+  }.
 
 moves([left, right, up, down, pass]).
 
 main :-
-  moves(Moves),
-  forall(
-    between(1, 40, _),
-    (
-      sleep(0.200),
-      random_member(Move, Moves),
-      format("~p~n", [Move])
-    )
-  ).
+  phrase_from_stream(game, current_input).
