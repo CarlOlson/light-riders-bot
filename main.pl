bfcda2ac190d988bfa3a2ff67a3cb57b73005cdb
CarlOlson <CarlOlson@users.noreply.github.com>
2018-05-23 00:52:30 -0500

    add current_player/1

--- main.pl
+++ main.pl
@@ -2,6 +2,8 @@
 :- use_module(library(dcg/basics)).
 :- use_module(library(pio)).
 
+:- dynamic settings/1.
+
 game --> commands, eos.
 
 commands --> command, commands.
@@ -11,16 +13,26 @@ command --> settings.
 command --> update.
 command --> move.
 
-settings --> "settings", string_without("\n", _), "\n".
+settings --> "settings ", string(List), " ", value(Value), "\n",
+	     {
+		 atom_codes(Key, List),
+		 asserta(settings(Key:Value))
+	     }.
 
 update   --> "update", string_without("\n", _), "\n".
 
 move     -->  "action move ", integer(_Time), "\n",
-  {
-    moves(Moves),
-    random_member(Move, Moves),
-    format("~p~n", [Move])
-  }.
+	      {
+		  moves(Moves),
+		  random_member(Move, Moves),
+		  format("~p~n", [Move])
+	      }.
+
+value(Value) --> integer(Value);
+		 string(List), { string_to_list(Value, List) }.
+
+current_player(Id) :-
+    settings(your_botid:Id).
 
 moves([left, right, up, down, pass]).
 
--- main_spec.pl
+++ main_spec.pl
@@ -1,5 +1,18 @@
 
+:- include(main).
 :- use_module(library(plspec)).
 
 :- describe(main).
+
+it("should parse arbitrary settings") :-
+    string_to_list("settings key value\n", List),
+    phrase(game, List),
+    settings(key:Value),
+    Value = "value".
+
+it("should parse botid setting") :-
+    string_to_list("settings your_botid 999\n", List),
+    phrase(game, List),
+    current_player(999).
+
 :- end(main).
